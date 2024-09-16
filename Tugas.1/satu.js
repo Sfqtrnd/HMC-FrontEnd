4704a3c0d5a1f7b0987c73c3ce80cbadac3cc893
Sfqtrnd <Sofiaqatrunnada06@gmail.com>
2024-09-16 16:22:44 +0800

    Tugas.1

---
+++ Tugas.1/satu.js
@@ -0,0 +1,16 @@
+console.log('hello world!');
+let nama = "Sofia Qatrunnada";
+let usia = 18;
+let apakahpelajar = "ya";
+
+console.log("nama:", nama)
+console.log("umur:", usia)
+console.log("apakah pelajar:", apakahpelajar)
+
+const a = 10;
+const b = 5;
+const c = a + b;
+console.log("10+5=", c)
+
+
+
